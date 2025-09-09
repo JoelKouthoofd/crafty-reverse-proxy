3908e9ed736396cb69cc139932ae9dc693013e45
Joel Kouthoofd <44805078+JoelKouthoofd@users.noreply.github.com>
2025-09-09 21:02:16 +0200

    Update playerCounter.go
    Remove Auto shutdown

--- playerCounter.go
+++ playerCounter.go
@@ -23,37 +23,3 @@ func getPlayerMap() *map[string]int {
 
 	return playerMap
 }
-
-func decrementPlayerCount(server ServerType) {
-	(*getPlayerMap())[indexFromServer(server)]--
-	if isServerEmpty(server) {
-		scheduleStopServerIfEmpty(server)
-	}
-}
-
-func incrementPlayerCount(server ServerType) {
-	(*getPlayerMap())[indexFromServer(server)]++
-}
-
-func isServerEmpty(server ServerType) bool {
-	return (*getPlayerMap())[indexFromServer(server)] == 0
-}
-
-func scheduleStopServerIfEmpty(server ServerType) {
-	if(getConfig().AutoShutdown == false){
-		return
-	}
-	time.AfterFunc(time.Duration(getConfig().Timeout)*time.Minute, func() {
-		stopServerIfEmpty(server)
-})
-}
-
-func stopServerIfEmpty(server ServerType){
-	internalPort :=server.InternalPort
-	if isServerEmpty(server){
-		port, err :=strconv.Atoi(internalPort)
-		if(err!=nil){
-			println("Expected number port but got: " + internalPort+"\n"+err.Error()+"\n")		}
-		stopMcServer(port)
-	}
-}
